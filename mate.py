f66c9fa5d18c07165d1eed197770a4eb6f2515f4
Juan Camilo Velez <jcavelez@gmail.com>
2021-04-12 16:40:47 -0500

    UI en PyQt5

--- mate.py
+++ mate.py
@@ -1,18 +1,24 @@
 '''
-
 @author: Juan Camilo Vélez
 '''
 #======================
 # imports
 #======================
+import sys
+from PyQt5 import uic, QtWidgets
 
+qtCreatorFile = 'main.ui'   #Archivo creado mediante Designer de PyQt5
+Ui_MainWindow, QtBaseClass = uic.loadUiType(qtCreatorFile) #El modulo ui carga el archivo
+app = QtWidgets.QApplication(sys.argv)
 
-
-def run():
-    pass
+class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
+    def __init__(self) -> None:
+        super().__init__()
+        QtWidgets.QMainWindow.__init__(self)
+        Ui_MainWindow.__init__(self)
+        self.setupUi(self)
 
 if __name__ == '__main__':
-    run()
-
-
-
+    main_window = MainWindow()
+    main_window.show()
+    sys.exit(app.exec_())
