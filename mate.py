02164bc9d02993ab503ea425ee5f45b447e54c2d
Juan Camilo Velez <jcavelez@gmail.com>
2021-04-13 13:50:01 -0500

    nueva interface

--- mate.py
+++ mate.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 '''
 @author: Juan Camilo Vélez
 '''
@@ -11,8 +13,14 @@ qtCreatorFile = 'main.ui'   #Archivo creado mediante Designer de PyQt5
 Ui_MainWindow, QtBaseClass = uic.loadUiType(qtCreatorFile) #El modulo ui carga el archivo
 app = QtWidgets.QApplication(sys.argv)
 
+def service_shutdown(signum, frame):
+    '''
+    Método para detener el Thread de crearConvertidor mediante el lanzamiento de una excepción
+    '''
+    raise ServiceExit
+
 class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
-    def __init__(self) -> None:
+    def __init__(self):
         super().__init__()
         QtWidgets.QMainWindow.__init__(self)
         Ui_MainWindow.__init__(self)
